e28e7b2a98712c08f1f616ba8cdf4ac9badeed0b
DenisSalt <skvinyt@yandex.ru>
2023-09-28 19:45:48 +0700

    Writing home work

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// //Задача 2
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine()); 
+// Console.Write("Введите число: ");
+// int b = Convert.ToInt32(Console.ReadLine()); 
+// if (a > b) {
+//     Console.Write("max a = " + a);
+// }
+// if (a < b) {
+//     Console.Write("max b = " + b);
+// }
+
+//Задача 4
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// if (a > b && a > c)
+// 	Console.Write(a);
+// else if (b > a && b > c)
+// 	Console.Write(b);
+// else
+// 	Console.Write(c);
+
+//Задача 6
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a % 2 == 0)
+//     Console.Write("Да");
+// else 
+//     Console.Write("Нет");
+
+//Задача 8
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++) {
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+} 
+//Задача 10
